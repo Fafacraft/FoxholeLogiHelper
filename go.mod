b92546338db16d2ed4c1b30c49c29e06e4680e8b
Fafacraft <fafa24252425@yahoo.com>
2023-06-05 19:37:27 +0200

    [ADD] working db

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module FLH
 
 go 1.19
+
+require github.com/mattn/go-sqlite3 v1.14.17
--- internal/stockpile/handler.go
+++ internal/stockpile/handler.go
@@ -1,23 +1,41 @@
 package stockpile
 
 import (
+	"database/sql"
+	"fmt"
 	"html/template"
 	"net/http"
 
 	"FLH/pkg/utils"
 
 	"FLH/internal/common"
+
+	_ "github.com/mattn/go-sqlite3"
 )
 
 type DataStockpile struct {
-	ItemBoxList []template.HTML
+	ItemBoxList01 []template.HTML
+	ItemBoxList02 []template.HTML
+	ItemBoxList03 []template.HTML
+	ItemBoxList04 []template.HTML
+	ItemBoxList05 []template.HTML
+	ItemBoxList06 []template.HTML
+	ItemBoxList07 []template.HTML
+	ItemBoxList08 []template.HTML
 }
 
 type DataItemBox struct {
 	Id        string
-	Title     string
-	ItemNb    int
+	Name      string
+	Category  string
+	Priority  int
+	Nb        int
+	NbNeeded  int
 	ImageLink string
+	BMats     int
+	RMats     int
+	EMats     int
+	HeMats    int
 }
 
 /*
@@ -32,35 +50,68 @@ func HandleStockpile(w http.ResponseWriter, r *http.Request) {
 	utils.CheckErr(err)
 
 	stockpile := DataStockpile{}
-	stockpile.ItemBoxList = makeItemBoxList()
+	// prepare different list with different priorities
+	stockpile.ItemBoxList01 = makeItemBoxList("priority", 1)
+	stockpile.ItemBoxList02 = makeItemBoxList("priority", 2)
+	stockpile.ItemBoxList03 = makeItemBoxList("priority", 3)
+	stockpile.ItemBoxList04 = makeItemBoxList("priority", 4)
+	stockpile.ItemBoxList05 = makeItemBoxList("priority", 5)
+	stockpile.ItemBoxList06 = makeItemBoxList("priority", 6)
+	stockpile.ItemBoxList07 = makeItemBoxList("priority", 7)
+	stockpile.ItemBoxList08 = makeItemBoxList("priority", 8)
+
 	dataIndex.Main = common.Execute(tpl, stockpile)
 
 	common.ExecuteFinishPage(w, dataIndex)
 }
 
-func makeItemBoxList() []template.HTML {
+/*
+list all items with appropriate :
+- all
+- priority  (int)
+- category  (string)
+*/
+func makeItemBoxList(option string, value any) []template.HTML {
 	finalList := []template.HTML{}
 
-	// get the db then for each item
-	itemBox := DataItemBox{}
-	itemBox.Id = "bMat"
-	itemBox.Title = "Basic Materials"
-	itemBox.ItemNb = 5
-	itemBox.ImageLink = "./static/images"
-	tplRaw, err := template.ParseFiles("static/templates/stockpile/itemBox.html")
+	// open the db
+	db, err := sql.Open("sqlite3", "./database.db")
 	utils.CheckErr(err)
-	tpl := *common.Execute(tplRaw, itemBox)
-	finalList = append(finalList, tpl)
+	// do the request with appropriate option
+	var rows *sql.Rows
+	switch option {
+	case "all":
+		preparedRequest, err := db.Prepare("SELECT * FROM stockpile")
+		utils.CheckErr(err)
+		rows, err = preparedRequest.Query()
+		break
+	case "priority":
+		preparedRequest, err := db.Prepare("SELECT * FROM stockpile WHERE priority == ?")
+		utils.CheckErr(err)
+		rows, err = preparedRequest.Query(value)
+		break
+	case "category":
+		preparedRequest, err := db.Prepare("SELECT * FROM stockpile WHERE category == ?")
+		utils.CheckErr(err)
+		rows, err = preparedRequest.Query(value)
+		break
+	default:
+		fmt.Println("ERROR : Unreconized option in makeItemBoxList()")
+	}
+	defer rows.Close() // THE THING INSIDE DEFER IS EVALUATED HERE, EVEN IF IT RUNS AT THE END
 
-	itemBox2 := DataItemBox{}
-	itemBox2.Id = "gas_mask"
-	itemBox2.Title = "Gas Mask"
-	itemBox2.ItemNb = 3
-	itemBox2.ImageLink = "./static/images"
-	tplRaw2, err := template.ParseFiles("static/templates/stockpile/itemBox.html")
-	utils.CheckErr(err)
-	tpl2 := *common.Execute(tplRaw2, itemBox2)
-	finalList = append(finalList, tpl2)
+	// get each item resulting
+	for rows.Next() {
+		itemBox := DataItemBox{}
+		err = rows.Scan(&itemBox.Id, &itemBox.Name, &itemBox.Category, &itemBox.Priority, &itemBox.Nb, &itemBox.NbNeeded, &itemBox.ImageLink, &itemBox.BMats, &itemBox.RMats, &itemBox.EMats, &itemBox.HeMats)
+		utils.CheckErr(err)
+
+		tplRaw, err := template.ParseFiles("static/templates/stockpile/itemBox.html")
+		utils.CheckErr(err)
+		tpl := *common.Execute(tplRaw, itemBox)
+		finalList = append(finalList, tpl)
+	}
+	fmt.Println(finalList)
 
 	return finalList
 }
