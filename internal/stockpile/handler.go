6167cccff236f6bad475e8b105c33309c9424fec
Fafacraft <fafa24252425@yahoo.com>
2023-06-05 17:36:56 +0200

    Ready to add multiple itemBox

--- internal/stockpile/handler.go
+++ internal/stockpile/handler.go
@@ -10,7 +10,14 @@ import (
 )
 
 type DataStockpile struct {
-	Id string
+	ItemBoxList []template.HTML
+}
+
+type DataItemBox struct {
+	Id        string
+	Title     string
+	ItemNb    int
+	ImageLink string
 }
 
 /*
@@ -20,12 +27,40 @@ Handle the stockpile page
 func HandleStockpile(w http.ResponseWriter, r *http.Request) {
 	// TODO : bla bla fill indexData, then executeFinishPage
 	dataIndex := common.DataIndex{}
+
 	tpl, err := template.ParseFiles("static/templates/stockpile/stockpile.html")
 	utils.CheckErr(err)
 
 	stockpile := DataStockpile{}
-	stockpile.Id = "bMat"
+	stockpile.ItemBoxList = makeItemBoxList()
 	dataIndex.Main = common.Execute(tpl, stockpile)
 
 	common.ExecuteFinishPage(w, dataIndex)
+}
+
+func makeItemBoxList() []template.HTML {
+	finalList := []template.HTML{}
+
+	// get the db then for each item
+	itemBox := DataItemBox{}
+	itemBox.Id = "bMat"
+	itemBox.Title = "Basic Materials"
+	itemBox.ItemNb = 5
+	itemBox.ImageLink = "./static/images"
+	tplRaw, err := template.ParseFiles("static/templates/stockpile/itemBox.html")
+	utils.CheckErr(err)
+	tpl := *common.Execute(tplRaw, itemBox)
+	finalList = append(finalList, tpl)
+
+	itemBox2 := DataItemBox{}
+	itemBox2.Id = "gas_mask"
+	itemBox2.Title = "Gas Mask"
+	itemBox2.ItemNb = 3
+	itemBox2.ImageLink = "./static/images"
+	tplRaw2, err := template.ParseFiles("static/templates/stockpile/itemBox.html")
+	utils.CheckErr(err)
+	tpl2 := *common.Execute(tplRaw2, itemBox2)
+	finalList = append(finalList, tpl2)
+
+	return finalList
 }
